2bd6b0f63fede1e787a556f322b8447645d7ad93
Sebastian Göttschkes <sebastian@diagnosia.com>
2020-11-11 09:16:02 +0100

    Add test.watch

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule Intercom.MixProject do
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.1"},
       {:mox, "~> 1.0", only: :test},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
