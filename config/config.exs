228b79f32618190fe063e45bb9f6fb7fa6b9d5c8
Sebastian Göttschkes <sebastian@diagnosia.com>
2020-11-11 09:05:39 +0100

    Add way to add temporary config values using the secret file

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,7 @@ use Mix.Config
 config :intercom, :http_adapter, HTTPoison
 
 import_config "#{Mix.env()}.exs"
+
+if File.exists?("config/#{Mix.env()}.secret.exs") do
+  import_config "#{Mix.env()}.secret.exs"
+end
