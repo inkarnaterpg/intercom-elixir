fc22c2990c39b8a58e092564c8dde30d1119f257
Sebastian Göttschkes <sebastian@diagnosia.com>
2020-11-16 16:09:52 +0100

    Update mix for hex release

--- mix.exs
+++ mix.exs
@@ -4,11 +4,17 @@ defmodule Intercom.MixProject do
   def project do
     [
       app: :intercom,
-      version: "1.0.0",
+      version: "1.3.0",
       elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: description(),
+      package: package(),
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      name: "intercom_elixir",
+      source_url: "https://github.com/diagnosia/intercom-elixir"
     ]
   end
 
@@ -25,11 +31,24 @@ defmodule Intercom.MixProject do
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.1"},
       {:mox, "~> 1.0", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
+  defp description do
+    "An Elixir library for working with Intercom using the Intercom API."
+  end
+
   defp elixirc_paths(:test), do: ["test/support", "lib"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      name: "intercom_elixir",
+      files: ~w(lib .formatter.exs mix.exs README.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/diagnosia/intercom-elixir"}
+    ]
+  end
 end
