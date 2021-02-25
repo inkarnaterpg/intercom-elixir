71d37d1ef25bcbba71335c1f71a6bdfd0f70532d
Sebastian Göttschkes <sebastian@diagnosia.com>
2021-02-25 19:40:23 +0100

    Update to version 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Intercom.MixProject do
   def project do
     [
       app: :intercom,
-      version: "1.3.0",
+      version: "2.0.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
